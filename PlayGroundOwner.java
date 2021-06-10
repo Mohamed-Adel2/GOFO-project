1c796e61286ce6145a0bfc7ca44031cd6cd888b8
sayed_4 <61192867+sayedshaaban4@users.noreply.github.com>
2021-06-10 23:06:33 +0200

    version-1.0

--- PlayGroundOwner.java
+++ PlayGroundOwner.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class for playground owner that can set his information,
+ * and his requests to his playgrounds,
+ * he can accept them or deny them .
  */
 package main;
 
@@ -9,7 +9,13 @@ package main;
  *
  * @author OWNER
  */
+
 import java.util.ArrayList;
+
+/**
+ *
+ * @author sayed
+ */
 
 public class PlayGroundOwner implements User {
     private ArrayList<Request> PR = new ArrayList<Request>();
@@ -19,14 +25,18 @@ public class PlayGroundOwner implements User {
     private String Phone;
     private String Location;
     private int ID;
-     //ArrayList<String> cars = new ArrayList<String>();
     private ArrayList<PlayGround> PG = new ArrayList<PlayGround>();
-    //private PlayGround PGG;
   
     
-//    public PlayGroundOwner(){
-//        ArrayList<PlayGround> PG = ;
-//    }
+    /**
+     *
+     * @param name , playground owner name.
+     * @param password , playground owner password.
+     * @param Email , playground owner email.
+     * @param Phone , playground owner phone.
+     * @param Location , playground owner location.
+     * @param ID , playground owner ID.
+     */
     public PlayGroundOwner(String name,String password, String Email, String Phone, String Location, int ID) {
         this.name=name;
         this.password = password;
@@ -34,60 +44,124 @@ public class PlayGroundOwner implements User {
         this.Phone = Phone;
         this.Location = Location;
         this.ID = ID;
-//        ArrayList<PlayGround> PG=new ArrayList<PlayGround>();
     }
 
+    /**
+     *
+     * @param name , update playground owner name.
+     */
     public void UpdateName(String name) {
         this.name = name;
     }
     
+    
+    /**
+     *
+     * @param password , update playground owner password.
+     */
     public void UpdatePassword(String password) {
         this.password = password;
     }
 
+    
+    /**
+     *
+     * @param Email , update playground owner email.
+     */
     public void UpdateEmail(String Email) {
         this.Email = Email;
     }
 
+    
+    /**
+     *
+     * @param Phone , update playground owner phone.
+     */
     public void UpdatePhone(String Phone) {
         this.Phone = Phone;
     }
 
+    
+    /**
+     *
+     * @param Location , update playground owner location.
+     */
     public void UpdateLocation(String Location) {
         this.Location = Location;
     }
 
+    
+    /**
+     *
+     * @param ID , update playground owner ID.
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    
+    /**
+     *
+     * @return , get playground owner name.
+     */
     public String getName() {
         return name;
     }
     
+    
+    /**
+     *
+     * @return , get playground owner password.
+     */
     public String getPassword() {
         return password;
     }
 
+    
+    /**
+     *
+     * @return , get playground owner email.
+     */
     public String getEmail() {
         return Email;
     }
 
+    
+    /**
+     *
+     * @return , get playground owner phone.
+     */
     public String getPhone() {
         return Phone;
     }
 
+    
+    /**
+     *
+     * @return , get playground owner location.
+     */
     public String getLocation() {
         return Location;
     }
 
+    
+    /**
+     *
+     * @return , get playground owner ID.
+     */
     public int getID() {
         return ID;
     }
 
+    
+    /**
+     *
+     * @param PGG , add playground.
+     */
     public void AddPG(PlayGround PGG){
         PG.add(PGG);
     }
+    
     
     void viewPlayGrounds(){
         System.out.println("PlayGroundOwner Name: "+getName());
@@ -100,12 +174,23 @@ public class PlayGroundOwner implements User {
         }
     }
 
+    
+    /**
+     *
+     * @param R , player request added here.
+     */
     public void AddRequest(Request R){
         PR.add(R);
         System.out.println('\n'+"Request was sent to PlayGround Owner he will answer soon, Please wait for response! ");
         System.out.println("Reply in progress......! " + '\n');
         AcceptRequest(R);
     }
+
+    
+    /**
+     *
+     * @param R , function take player request and accept or deny it.
+     */
     public void AcceptRequest(Request R){
         int indx=0;
         int flag=0,flagg=0;
@@ -136,19 +221,28 @@ public class PlayGroundOwner implements User {
                 PG.get(i).avaliablehours.remove(indx);
                 PG.get(i).avaliablehours.add(indx,x);
                 flagg=1;
-                if(flagg==1){System.out.println("Request Accepted!.");PR.remove(R);}
-                else DenyRequest(R);
-                
+                if(flagg==1)System.out.println("Request Accepted!.");
+                else System.out.println("Request Denied!.");
+                PR.remove(R);
         }
-        
-  
         }
     }
 }
+
+    
+    /**
+     *
+     * @param R , deny request function.
+     */
     public void DenyRequest(Request R){
         System.out.println("Request Denied!.");
         PR.remove(R);
     }
+
+    
+    /**
+     * display the requests of playground have.
+     */
     public void viewReq(){
         for(int i=0;i<PR.size();i++){
            PR.get(i).viewRequest();
