af93f0f37379c348811c8fe2dab15ee8fa72745f
sayed_4 <61192867+sayedshaaban4@users.noreply.github.com>
2021-06-10 23:07:33 +0200

    version-1.0

--- Player.java
+++ Player.java
@@ -1,15 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* This class for player that can set his information,
+* and also can requests a playground in specific day and time.
+*
+*/
 package main;
 
 /**
  *
  * @author OWNER
  */
+
 import java.util.ArrayList;
+
+/**
+ *
+ * @author sayed
+ */
+
 public class Player implements User{
     private ArrayList<Request> PR = new ArrayList<Request>();
     public PlayGrounds ps;
@@ -20,9 +27,16 @@ public class Player implements User{
     private String Location;
     private int ID;
 
-    //public PlayerRequest PR;
-
-
+    /**
+     *
+     * @param ps , this is object from all playgrounds.
+     * @param name , the name of the player.
+     * @param password , the password of the player.
+     * @param Email , the email of the player.
+     * @param Phone , the phone of the player.
+     * @param Location , the location of the player.
+     * @param ID , the ID of the player.
+     */
     public Player(PlayGrounds ps,String name, String password, String Email, String Phone, String Location, int ID) {
         this.name = name;
         this.password = password;
@@ -33,60 +47,128 @@ public class Player implements User{
         this.ps=ps;
     }
 
+    
+    /**
+     *
+     * @param name , update player name.
+     */
     public void UpdateName(String name){
         this.name = name;
     }
 
+    
+    /**
+     *
+     * @param password , update player password.
+     */
     public void UpdatePassword(String password) {
         this.password = password;
     }
 
+    /**
+     *
+     * @param Email , update player email.
+     */
     public void UpdateEmail(String Email) {
         this.Email = Email;
     }
 
+    
+    /**
+     *
+     * @param Phone , update player phone.
+     */
     public void UpdatePhone(String Phone) {
         this.Phone = Phone;
     }
 
+    
+    /**
+     *
+     * @param Location , update player location.
+     */
     public void UpdateLocation(String Location) {
         this.Location = Location;
     }
 
+    
+    /**
+     *
+     * @param ID , update player ID.
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    
+    /**
+     *
+     * @return , get the name of the player.
+     */
     public String getName() {
         return name;
     }
 
+    
+    /**
+     *
+     * @return , get the password of the player.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return , get the email of the player.
+     */
     public String getEmail() {
         return Email;
     }
 
+    /**
+     *
+     * @return , get the phone of the player.
+     */
     public String getPhone() {
         return Phone;
     }
 
+    /**
+     *
+     * @return , get the location of the player.
+     */
     public String getLocation() {
         return Location;
     }
 
+    /**
+     *
+     * @return , get the ID of the player.
+     */
     public int getID() {
         return ID;
     }
+
+    /**
+     *
+     * @param R , set request to specific playground and pass it to playground owner. 
+     */
     public void AddRequest(Request R){
         PR.add(R);
         System.out.println("Request Sent Successfuly! ");
     }
+
+    /**
+     * display all playgrounds.
+     */
     public void ViewPs(){
         ps.ViewPlayGrounds();
     }
+
+    /**
+     * display my requests.
+     */
     public void viewReq(){
         for (int i=0;i<PR.size();i++){
             PR.get(i).viewRequest();
