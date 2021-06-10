5ce43f44a445683f53e4008e31004164e6bf0867
sayed_4 <61192867+sayedshaaban4@users.noreply.github.com>
2021-06-10 23:08:01 +0200

    version-1.0

--- Request.java
+++ Request.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class for any request that the player make.
+ * 
+ * 
  */
 package main;
 
@@ -9,8 +9,16 @@ package main;
  *
  * @author OWNER
  */
+
 import java.util.ArrayList;
+
+/**
+ *
+ * @author sayed
+ */
+
 public class Request {
+
     public PlayGrounds ps;
     public String s;
     int[] Hours;
@@ -18,6 +26,14 @@ public class Request {
     public PlayGround PG;
     public Player P;
 
+    /**
+     *
+     * @param ID , The ID refer to the playground. 
+     * @param Hours , The hours that the player will request to reserve it. 
+     * @param P , The player who make the request.
+     * @param s , The day that the player will request.
+     * @param ps , The playgrounds.
+     */
     public Request(int ID ,int[] Hours, Player P,String s, PlayGrounds ps){
     this.ID=ID;
     this.P=P;
@@ -29,20 +45,36 @@ public class Request {
     this.ps=ps;
     setPG();
     } 
+
+    /**
+     * set playground equal to the ID of playground that will be requested.
+     */
     public void setPG(){
         for(int i=0; i<ps.PG.size() ;i++){
             if(ps.PG.get(i).getId()==this.ID)
                 PG=ps.PG.get(i);
         }
     }
+
+    /**
+     * display all requests.
+     */
     public void viewRequest(){
         System.out.println("PlayGround ID: "+ID);
         System.out.println("Player Name: "+P.getName());
         System.out.println("Booking Day: "+s);
     }
+
+    /**
+     * set this request to the plater requests.
+     */
     public void AddRequesttoPlayer(){
         P.AddRequest(this);
     }
+
+    /**
+     * set this request to the playdround owner requests.
+     */
     public void AddRequesttoPGO(){
         PG.getPGO().AddRequest(this);
     }
